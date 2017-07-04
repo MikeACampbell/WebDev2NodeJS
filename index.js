79465a0a76f8526ec873f7767f7517cbfd7b90e6
MikeACampbell <mikecampbella@gmail.com>
2017-07-03 21:00:14 -0500

    Added Milestone 2

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 
 var pg = require("pg"); // This is the postgres database connection module.
 //const connectionString = "postgres://ta_user:ta_pass@localhost:5433/onlinestore";
-const connectionString = DATABASE_URL;
+const connectionString = process.env.DATABASE_URL;
 
 app.get('/getItem', function(request, response) {
 	getItem(request, response);
