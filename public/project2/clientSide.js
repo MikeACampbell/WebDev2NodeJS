377a0a6b0a55d3d6a4b91596f3b1ab1f29621166
MikeACampbell <mikecampbella@gmail.com>
2017-07-03 21:06:07 -0500

    Added Milestone 2

--- public/project2/clientSide.js
+++ public/project2/clientSide.js
@@ -1,7 +1,7 @@
 $(document).on('click', "#store-button", function () {
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:5000/getItemsFromDb',
+        url: process.env.DATABASE_URL,
 		success: function (result) {
 		
 		document.getElementById("divResults").innerHTML = result;
@@ -14,7 +14,7 @@ $(document).on('click', "#store-button", function () {
 $(document).on('click', "#order-button", function () {
     $.ajax({
         type: 'GET',
-        url: 'http://localhost:5000/getOrdersFromDb',
+        url: 'https://localhost:5000/getOrdersFromDb',
 		success: function (result) {
 		
 		document.getElementById("divResults").innerHTML = result;
