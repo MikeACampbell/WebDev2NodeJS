a1e320e1d82c18600bf4c429aeec6aaeaa2f46d7
Michael Campbell <Mikecampbella@gmail.com>
2017-07-18 05:31:25 -0600

    Added insert to Order table

--- index.js
+++ index.js
@@ -114,6 +114,19 @@ app.post('/verifyOrder', verify, function(req, response) {
 						console.log(allItems);
 					}
 				});		
+						
+			});
+
+
+			var client = new pg.Client(connectionString);
+		client.connect(function(err) {
+			if (err) {
+				console.log("Error connecting to DB: ")
+				console.log(err);
+				callback(err, null);
+			}
+
+			
 			
 	var sql = "INSERT INTO orders (items, userid, price, ordereddate, status) VALUES ($1, $2, $3, CURRENT_DATE, 0)";
 	var query = client.query(sql, [allItems, request.session.user_id, total], function(err, result) {
@@ -133,12 +146,8 @@ app.post('/verifyOrder', verify, function(req, response) {
 				
 			}
 						
-	});
-				
-				
-	});
-
-				
+				});
+		});		
 });
 		
 
